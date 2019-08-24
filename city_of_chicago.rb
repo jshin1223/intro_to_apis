1a0c7c494e3b339c4869cc8ca09242e31c8c26e0
jshin1223 <jshin1223@gmail.com>
2019-08-24 10:32:48 -0500

    revision to city_of_chicago

--- city_of_chicago.rb
+++ city_of_chicago.rb
@@ -2,19 +2,21 @@ require "http"
 
 response = HTTP.get("http://data.cityofchicago.org/resource/xzkq-xp2w.json")
 
-pp response.parse
-
-# city_employees = response.parse
 
 
-# city_employees.each do |city_employee|
-#   puts "Name: #{response.parse[["name"]}"
-#   puts "Job Title: #{response.parse["job_titles"]}"
-#   puts "Department: #{response.parse["department"]}"
-#   puts "Salary: $#{response.parse["annual salary"]}"
-#   puts ""
-#   puts "-" * 50
-#   puts ""
-# end
+city_employees = response.parse
+
+
+
+
+city_employees.each do |city_employee|
+  puts "Name: #{city_employee["name"]}"
+  puts "Job Title: #{city_employee["job_titles"]}"
+  puts "Department: #{city_employee["department"]}"
+  puts "Salary: $#{city_employee["annual_salary"]}"
+  puts ""
+  puts "-" * 50
+  puts ""
+end
 
 
